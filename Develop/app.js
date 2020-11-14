02faa0ca89c152b3fd6ac5987ebf6a51b912f52e
Rachel Moore <moorer@gcsnc.com>
2020-11-14 13:43:26 -0500

    added write to html file

--- Develop/app.js
+++ Develop/app.js
@@ -68,12 +68,13 @@ const promptUser = () => {
   }
     ])
   .then(answers => {
-    console.log(role)
+    console.log(answers.role)
     let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
     // Pushing manager object to teams array
     Team.push(manager)
     console.log(answers)
 
+    makeTeam()
   })
     
 }
@@ -153,6 +154,12 @@ const promptEngineer = () => {
 
 
   promptUser()
+
+
+  const makeTeam = () => {
+    fs.writeFileSync(outputPath, render(Team), "utf-8")
+
+  }
   // .then(answers => {
     // const html = generateHTML(answers);
     // return writeFileAsync('index.html', html);
@@ -241,7 +248,6 @@ const promptEngineer = () => {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -266,11 +272,9 @@ const promptEngineer = () => {
       </div>
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
-
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
     <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
